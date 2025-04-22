5cfaad2322263ac2c743974d6f8e268d66cee056
unwraps <forbf100@gmail.com>
2025-04-22 16:33:37 +0700

    a10

--- src/app.test.js
+++ src/app.test.js
@@ -10,3 +10,8 @@ test('getDay returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
     expect( month ).toBe('March');
 });
+
+test('getDay returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('March');
+});
